0e4fd6b1d37adfab6aa504a9eafb475f05f63e40
Terris <27739101+Terristwj@users.noreply.github.com>
2023-05-30 16:28:22 +0800

    Setup

---
+++ Workshop Items/JavaWorkshop_PreReading/src/Exercise/Exercise1.java
@@ -0,0 +1,20 @@
+package Exercise;
+
+//TODO: import scanner
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        //TODO: create scanner
+
+        //TODO: prompt for user's name and store in a variable
+
+        //TODO: prompt for user's weight and store in a variable
+
+        //TODO: prompt for user's height and store in a variable
+
+        //TODO: calculate BMI annd store in a variable
+        // use weight divided by height squared
+
+        //TODO: print the result
+    }
+}
