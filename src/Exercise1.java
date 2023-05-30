209d3ef95ed523028e4b478f5e0eae28e6babd18
Terris <27739101+Terristwj@users.noreply.github.com>
2023-05-30 17:23:12 +0800

    Finished Ex1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,20 +1,29 @@
-package Exercise;
-
 //TODO: import scanner
+import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
         //TODO: create scanner
+        Scanner sc = new Scanner(System.in);
 
         //TODO: prompt for user's name and store in a variable
+        System.out.print("Give me your name:\t");
+        String name = sc.nextLine();
 
         //TODO: prompt for user's weight and store in a variable
+        System.out.print("Now your weight:\t");
+        float weight = sc.nextFloat();
 
         //TODO: prompt for user's height and store in a variable
+        System.out.print("Height?\t\t");
+        float height = sc.nextFloat();
 
         //TODO: calculate BMI annd store in a variable
         // use weight divided by height squared
+        float bmi = weight/(height*height);
 
         //TODO: print the result
+        System.out.print(name + ", youe BMI is:\t" + bmi);
+        sc.close();
     }
 }
